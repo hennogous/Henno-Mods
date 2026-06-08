09c3cfe52f17108e7af6d298c5a5192d99665fe6
hennogous <henno.gous@gmail.com>
2026-06-08 15:49:06 +0200

    Removed Archive from repo

--- Archive/Mods/Henno Custom Adjacency Rework Mod/HCAR_DB.sql
+++
@@ -1,4 +0,0 @@
---==========================================================================================================================
--- Zegangani: adding new 'MinRings' Column to table 'Ruivo_New_Adjacency'
---==========================================================================================================================
-ALTER TABLE Ruivo_New_Adjacency ADD COLUMN MinRings INTEGER NOT NULL DEFAULT 1;
